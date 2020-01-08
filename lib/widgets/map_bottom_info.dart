82e954a513309c7ffd28a6101d0ea9ef59bb9a34
ialexies <ialexies@gmail.com>
2020-01-08 15:52:17 +0800

    with sample  modal for driver

--- lib/widgets/map_bottom_info.dart
+++ lib/widgets/map_bottom_info.dart
@@ -1,3 +1,5 @@
+// import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:byahero/states/mapstate.dart';
 import 'package:provider/provider.dart';
@@ -7,7 +9,7 @@ Positioned mapBottomInfo({context,screeWidth,Widget content, var1, var2, var3}){
     Positioned  (
       bottom: 0,
       // height: 60,
-      width: screeWidth/3,
+      // width: screeWidth/7,
       child: Visibility(
         // visible: var1, 
         // visible: MapState().destinationBottomInfo, 
@@ -15,11 +17,61 @@ Positioned mapBottomInfo({context,screeWidth,Widget content, var1, var2, var3}){
         visible: Provider.of<MapState>(context).destinationBottomInfo  ,
           child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Container(
-            color: Colors.black.withOpacity(.6),
-            child: content,
+          child: Row(
+            children: <Widget>[
+              Container(
+                color: Colors.black.withOpacity(.6),
+                child: content,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: FloatingActionButton(
+                  elevation: 30,
+                  onPressed: (){
+                    _showDialog(context: context);
+                  },
+                  child: Icon(Icons.drive_eta),
+                ),
+              )
+
+            ],
           ),
         ),
       ),
     );
 }
+
+ void _showDialog({context}) {
+   
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("List of Available Drivers"),
+          content:Container(
+            height: 150,
+            child: Column(
+              children: <Widget>[
+                Text('Alexies Iglesia'),
+                Text('Alexies Iglesia'),
+                Text('Alexies Iglesia'),
+                Text('Alexies Iglesia'),
+              ],
+            ),
+          ),
+
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
